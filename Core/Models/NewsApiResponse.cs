e3294b9e7e6e283fac7cec607d3d27cd9d50e898
David Browning <david.a.browning@gmail.com>
2026-02-01 21:46:22 +0100

    Fetch api data in console app.

---
+++ Core/Models/NewsApiResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class NewsApiResponse
+    {
+        public string Status { get; set; }
+        public int TotalResults { get; set; }
+        public List<Article> Articles { get; set; } = new();
+    }
+}
--- MauiNews.ConsoleApp/Program.cs
+++ MauiNews.ConsoleApp/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace MauiNews.ConsoleApp
+﻿using Core.Models;
+using NewsAPI;
+using NewsAPI.Constants;
+using NewsAPI.Models;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace MauiNews.ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            HttpClient httpClient = new();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("DavidsMauiNewsApp/1.0");
+            string apiKey = "96f9e2bd33a44349b2880c91352a9f66";
+            string url = $"https://newsapi.org/v2/everything?q=keyword&apiKey={apiKey}";
+            var response = await httpClient.GetAsync(url);
+            var news = await response.Content.ReadFromJsonAsync<NewsApiResponse>();
+            if (news != null && news.Status == "ok")
+            {
+                foreach (var article in news.Articles)
+                {
+                    Console.WriteLine($"{article.Title}");
+                }
+            }
         }
     }
 }
