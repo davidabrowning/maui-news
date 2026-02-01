7a475165762b335cbfb5588bae54c93ab95cbe13
David Browning <david.a.browning@gmail.com>
2026-02-01 22:09:38 +0100

    Add and show dummy articles.

--- MauiNews.MobileApp/ViewModels/MainPageViewModel.cs
+++ MauiNews.MobileApp/ViewModels/MainPageViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,10 @@ namespace MauiNews.MobileApp.ViewModels
     public class MainPageViewModel
     {
         public string Title { get; } = "Maui News";
+        public ObservableCollection<Article> Articles { get; } = new() {
+            new Article() {Title = "NYT Headline 1"},
+            new Article() {Title = "WSJ Headline 2"},
+            new Article() {Title = "Reuters Headline 3"},
+        };
     }
 }
