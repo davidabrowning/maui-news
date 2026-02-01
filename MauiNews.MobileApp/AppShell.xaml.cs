f1a1e541ec696377bbc530d37f81b41650eaf5bd
David Browning <david.a.browning@gmail.com>
2026-02-01 22:00:45 +0100

    Set up MAUI app.

---
+++ MauiNews.MobileApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiNews.MobileApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiNews.MobileApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MauiNews.MobileApp.ViewModels;
+
+namespace MauiNews.MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
