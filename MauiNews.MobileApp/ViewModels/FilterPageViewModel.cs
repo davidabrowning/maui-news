fc08a7bc0ab18761e0136098aef528c6a08f86b0
David Browning <david.a.browning@gmail.com>
2026-02-02 14:12:07 +0100

    Randomize filter terms.

--- MauiNews.MobileApp/ViewModels/FilterPageViewModel.cs
+++ MauiNews.MobileApp/ViewModels/FilterPageViewModel.cs
@@ -8,8 +8,22 @@ namespace MauiNews.MobileApp.ViewModels
 {
     public class FilterPageViewModel
     {
-        public string SearchTermA { get; set; } = "Java";
-        public string SearchTermB { get; set; } = "JavaScript";
-        public string SearchTermC { get; set; } = "NYC";
+        public string SearchTermA { get { return GetRandomSearchTerm(); } }
+        public string SearchTermB { get { return GetRandomSearchTerm(); } }
+        public string SearchTermC { get { return GetRandomSearchTerm(); } }
+
+        private List<string> SearchTerms = new() { "Health", "Sports", "NYC", "Stockholm", "Trump", "Mamdani", "Apple",
+        "Google", "ChatGPT", "AI", "Basketball", "Tech", "Literature", "Programming", "Music", "Piano", "Kristersson",
+        "Water", "Epidemics", "Influensa", "Covid", "Bicycles", "Cars", "Cooking", "Food", "Games", "Coffee",
+        "Pasta", "China", "Australia", "India", "Sweden", "France", "Stoicism", "Snow", "Handwriting", "Art" };
+
+        private string GetRandomSearchTerm()
+        {
+            int min = 0;
+            int max = SearchTerms.Count;
+            Random rand = new();
+            int index = rand.Next(min, max);
+            return SearchTerms.ElementAt(index);
+        }
     }
 }
